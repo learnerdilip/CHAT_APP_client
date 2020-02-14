098ff1c43ec5dded906291f888909f8fe878114c
learnerdilip <diliptalks@gmail.com>
2020-02-14 15:20:47 +0100

    added form for entering the values

--- src/App.js
+++ src/App.js
@@ -1,18 +1,72 @@
 import React, { Component } from "react";
 import "./App.css";
+import Loading from "./Loading";
+
 
 class App extends Component {
+  state = {
+    messages: [],
+    value: ""
+  };
+
   stream = new EventSource(`http://localhost:4000/stream`);
 
   componentDidMount() {
     this.stream.onmessage = event => {
       const parsed = JSON.parse(event.data);
-      console.log("event", parsed, typeof parsed);
+
+      console.log("PARSED", parsed.type, parsed.payload);
+      if (parsed.type === "MESSAGES") {
+        this.setState({
+          messages: parsed.payload
+        });
+      }
+      if (parsed.type === "MESSAGE") {
+        const messages = [parsed.payload, ...this.state.messages];
+        this.setState({
+          messages: messages
+        });
+      }
     };
   }
 
+  onChange = e => {
+    const { value } = e.target;
+    this.setState({ value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    
+  };
+
+  onClear = () => {
+    this.setState({
+      value: ""
+    });
+  };
+
   render() {
-    return <div className="App"> Hello</div>;
+    console.log("state", this.state);
+
+    if (!this.state.messages.length) return <Loading />;
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <input
+            value={this.state.value}
+            name="messages"
+            onChange={this.onChange}
+            type="text"
+          />
+          <button type="submit">SUBMIT</button>
+          <button onClick={this.onClear}>CLEAR</button>
+        </form>
+        {this.state.messages.map(mess => {
+          return <p>{mess.message} </p>;
+        })}
+      </div>
+    );
   }
 }
 
---
+++ src/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Loading() {
+  return (
+    <div>
+      <img
+        src="https://previews.123rf.com/images/estherpoon/estherpoon1706/estherpoon170600035/80108153-loading-icon.jpg"
+        alt="The loading image"
+      />
+    </div>
+  );
+}
